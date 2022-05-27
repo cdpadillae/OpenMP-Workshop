126d847cc2bc15ce54a563e3ef8927c420a9767e
DanielaDazaM <ddazam@unal.edu.co>
2022-05-27 06:46:17 -0500

    Plots of 12000 per thread

--- plot.py
+++ plot.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #def f(x):
-#    return x
-data = pd.read_csv('data_integral.txt',sep='\s+',header=None)
+ #   return x
+data = pd.read_csv('data_accuracy.txt',sep='\s+',header=None)
 data = pd.DataFrame(data)
 
 xtheo = np.linspace(0,16,16)
 x = data[0]
-y = 0.0209906/(data[1]*x)
+y = data[6]
 plt.xlabel("n threads")
-plt.ylabel("Parallel Efficiency")
-plt.title("Parallel Efficiency for Integral Calculation-Computer with 16 cores")
+plt.ylabel("Relative difference")
+plt.title("Relative difference vs the number of threads (16 cores)")
 
 plt.plot(x, y,'r')
 #plt.plot(xtheo,f(xtheo))
-plt.legend(["Exp N=10000000"])
+plt.legend(["Exp N=12000 per thread"])
 plt.show()
